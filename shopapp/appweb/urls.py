dba5ba54ca41776d3ed166c98e2c3ab73d027883
Stephen Lucic <st.lucic@duocuc.cl>
2024-09-19 18:15:26 -0300

    se actualiza mantenedor agregar de artista

--- shopapp/appweb/urls.py
+++ shopapp/appweb/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home,artistas, contacto
+from .views import home,artistas, contacto, agregar_artista
 
 urlpatterns = [
     path('home/', home, name="home"),
     path('', home, name="home"),
     path('artistas/',artistas, name="artistas"),
     path('contacto', contacto, name="contacto"),
-    path('mantenedor/agregar_artistas/',)
+    path('mantenedor/agregar_artistas/',agregar_artista, name="agregar_artistas")
 
 ]
